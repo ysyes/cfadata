2f948aba47ac44af239b97b511aacf4a23791ba3
Your Name <you@example.com>
2017-07-18 16:03:03 +0800

    test

---
+++ 1/index.js
@@ -0,0 +1,86 @@
+//点击生成数据
+$("#box div").click(function () {
+	//显示
+	$("#myteam").show();
+	
+	
+})
+//点击新增
+$("#title p").click(function () {
+	$("#title ul").append('<li><input class="name" type="text" value="" placeholder="name"/>:<input class="num" type="number" value="0" /><i title="删除">×</i></li>')
+});
+//删除
+$("#title ul").on("click","li i",function () {
+	$(this).parent().remove();
+})
+
+//点击提交生成
+$("#title div").click(function () {
+	var data = [];
+	var datas = [];
+	$(".name").each(function (idx) {
+		if ($(this).val() !== "" && $(".num").eq(idx).val() !== "") {
+			data.push({
+				value:$(".num").eq(idx).val(),
+				name:$(this).val()
+			});
+			datas.push({
+				name:$(this).val(),
+				icon:'circle'
+			});
+		}
+	});
+	if (data.length > 0) {
+		boxFn(data,datas);
+	}
+})
+//数据项
+/*var data= [
+	{value:12,name:"中超"},
+	{value:15,name:"中甲"}
+];
+var datas= [
+    {name:"中超",icon:'circle'},
+    {name:"中甲",icon:'circle'}
+]
+boxFn(data,datas);*/
+function boxFn(data,datas){
+    var lastoption = {
+        tooltip : {
+            trigger: 'item',
+            formatter: "{b} : {c}<br/>{d}%"
+        },
+        legend: {
+            orient: 'vertical',
+            left: '80%',
+            top:'middle',
+            right:'10%',
+            itemWidth:8,
+            itemGap:8,
+            data: datas
+        },
+        series : [
+            {
+                type: 'pie',
+                radius : '65%',
+                center: ['35%', '50%'],
+                right:20,
+                data:data,
+                label:{
+                    normal:{
+                        formatter:'{b}: {c}'
+                    }
+                },
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+    var last = echarts.init($('#box')[0]);
+    last.setOption(lastoption);
+}
